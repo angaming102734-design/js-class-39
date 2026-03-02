5c02f67cd48cd4e23aed9f709af6ff9ff3c648c3
ather ali <a.ngaming102734@gmail.com>
2026-03-03 02:05:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// // let numbers = new Set([1,2,3]);
+
+// // console.log(numbers);
+
+// let s = new Set ();
+
+// s.add(10);
+// s.add(20);
+// s.add(30);
+
+// // s.delete(20)
+
+
+// // console.log(s);
+
+// // s.clear()
+// let s = new Set ([1,23]);
+
+// for (let value of s ) {
+//      console.log(value);
+    
+//  }
+
+//  let arr1 = [1,2,34,3]
+
+//    let arr=[new(arr1)]
+
+// //   let un = [...new Set (arr)];
+
+//   console.log(arr);
+  
+// // s.d 
+
+// s.clear();
+
+// console.log(s);
+
+// let s = new Set([1,])
